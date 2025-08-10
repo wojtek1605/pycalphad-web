a866971085bd872629e6ca02f073e6230adfe628
Wojtek Gierlotka <wojtek@Wojteks-iMac.local>
2025-08-10 22:54:15 +0800

    corection

--- app.py
+++ app.py
@@ -1,12 +1,4 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Sun Aug 10 21:18:05 2025
-
-@author: wojtek
-"""
-
-from flask import Flask, request, send_file, jsonify
+from flask import Flask, request, send_file, render_template
 import matplotlib.pyplot as plt
 import io
 from pycalphad import Database, binplot
@@ -15,53 +7,34 @@ import pycalphad.variables as v
 app = Flask(__name__)
 
 @app.route('/')
-def home():
-    return jsonify({
-        "message": "PyCalphad API is running. Send a GET request to /calculate"
-    })
+def index():
+    return render_template('index.html')
 
-@app.route('/calculate', methods=['GET'])
+@app.route('/calculate')
 def calculate():
-    
+    system = request.args.get('system', 'ag_al')
 
-    db_agal = Database('ag_al.TDB')
-    phases = ['LIQUID','FCC_A1']
-    fig = plt.figure(figsize=(9,6))
-    axes = fig.gca()
-    binplot(db_agal, ['AG', 'AL', 'VA'] , phases, {v.X('AL'):(0,1,0.02), v.T: (300, 1500, 10), v.P:101325, v.N: 1}, plot_kwargs={'ax': axes})
+    # Map system param to database file and phases
+    if system == 'ag_al':
+        db_file = 'ag_al.TDB'
+        phases = ['LIQUID', 'FCC_A1']
+        comps = ['AG', 'AL', 'VA']
+    elif system == 'fe_mn':
+        db_file = 'fe_mn.TDB'
+        phases = ['LIQUID', 'BCC_A2']
+        comps = ['FE', 'MN', 'VA']
+    else:
+        return "Unknown system", 400
 
-   
-    
-    
-    
-    
-    
-    # Get user parameters from query string
-    #element = request.args.get('element', 'Fe')
-    #temperature = float(request.args.get('T', 1000))
+    db = Database(db_file)
+    fig, ax = plt.subplots(figsize=(9,6))
+    binplot(db, comps, phases, {v.X(comps[1]): (0, 1, 0.02), v.T: (300, 1500, 10), v.P:101325, v.N:1}, plot_kwargs={'ax': ax})
 
-    # ----------------------------
-    # PLACEHOLDER for pycalphad code
-    # Example: do real calculations here
-    # result = run_pycalphad(element, temperature)
-    # For now, let's just make a dummy plot
-    # ----------------------------
-    #temps = [temperature - 200, temperature, temperature + 200]
-    #values = [t**0.5 for t in temps]  # Fake data
-
-    #plt.figure()
-    #plt.plot(temps, values, marker='o')
-    #plt.title(f"PyCalphad Results for {element} at {temperature}K")
-    #plt.xlabel("Temperature (K)")
-    #lt.ylabel("Property Value")
-
-    # Save to bytes buffer
     buf = io.BytesIO()
-    plt.savefig(buf, format='png')
-    plt.close()
+    fig.savefig(buf, format='png')
+    plt.close(fig)
     buf.seek(0)
-
     return send_file(buf, mimetype='image/png')
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(debug=True)
