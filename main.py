61243e9e3eb04b199ca0583b6763f78e7220554a
Wojtek Gierlotka <wojtek@Wojteks-iMac.local>
2025-08-10 20:55:44 +0800

    initial commit for pycalphad web app

---
+++ main.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Dec  9 18:12:32 2023
+
+@author: wojtekgierlotka
+"""
+
+print('Hello1')
+print('test')
+print("new")
+
+import matplotlib.pyplot as plt
+from pycalphad import Database, binplot
+import pycalphad.variables as v
+
+db_agal = Database('ag_al.TDB')
+phases = ['LIQUID','FCC_A1']
+fig = plt.figure(figsize=(9,6))
+axes = fig.gca()
+binplot(db_agal, ['AG', 'AL', 'VA'] , phases, {v.X('AL'):(0,1,0.02), v.T: (300, 1500, 10), v.P:101325, v.N: 1}, plot_kwargs={'ax': axes})
+
+plt.show()
---
+++ test.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Dec  9 18:12:32 2023
+
+@author: wojtekgierlotka
+"""
+
+print('Hello')
+print('test')
